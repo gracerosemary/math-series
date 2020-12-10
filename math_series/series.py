a57ebe38a1880bcd56af9e15c6533d730a6e0fbb
Grace Choi <choi.g.330@gmail.com>
2020-12-09 20:47:04 -0800

    lucas sum series passing

--- math_series/series.py
+++ math_series/series.py
@@ -1,9 +1,30 @@
 def fibonacci(n):
-    if  n <=0:
+    if  n <= 0:
         print('Nope.')
-    elif n ==1:
+    elif n == 1:
         return 0 
     elif n == 2:
         return 1
     else:
         return(fibonacci(n-1)+fibonacci(n-2))
+
+def lucas(n):
+    if  n <= 0:
+        print('Nope.')
+    elif n == 1:
+        return 2 
+    elif n == 2:
+        return 1
+    else:
+        return(lucas(n-1)+lucas(n-2))
+
+def sum_series(n, x='optionOne', y = 'optionTwo'):
+    x = 0
+    y = 1
+    if not x == 2 & y == 1:
+        return lucas(n)
+    else:
+        return fibonacci(n)
+
+
+
