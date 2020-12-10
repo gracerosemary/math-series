291c71cf66f257de8eaf99b02812efc18d82d401
Grace Choi <choi.g.330@gmail.com>
2020-12-10 01:03:19 -0800

    refactored again

--- math_series/series.py
+++ math_series/series.py
@@ -1,41 +1,38 @@
 def fibonacci(n):
-    if  n <= 0:
-        print('Nope.')
-    elif n == 1:
-        return 0 
-    elif n == 2:
-        return 1
-    else:
-        return (fibonacci(n-1)+fibonacci(n-2))
+    return None if n <= 0 else 0 if n == 1 else 1 if n == 2 else (fibonacci(n-1)+fibonacci(n-2))
+    
+    # if  n <= 0:
+    #     return None
+    # elif n == 1:
+    #     return 0 
+    # elif n == 2:
+    #     return 1
+    # else:
+    #     return (fibonacci(n-1)+fibonacci(n-2))
 
 def lucas(n):
-    if  n <= 0:
-        print('Nope.')
-    elif n == 1:
-        return 2 
-    elif n == 2:
-        return 1
-    else:
-        return (lucas(n-1)+lucas(n-2))
+    return None if n <= 0 else 2 if n == 1 else 1 if n == 2 else (lucas(n-1)+lucas(n-2))
+    # if  n <= 0:
+    #     return None
+    # elif n == 1:
+    #     return 2 
+    # elif n == 2:
+    #     return 1
+    # else:
+    #     return (lucas(n-1)+lucas(n-2))
 
-def sum_series(n, x = 0, y = 1):
-    
-    defaultArgs = [
-        x == 0,
-        y == 1
-        ]
-    
-    optionalArgs = [
-        x == 2,
-        y == 1
-        ]
+def sum_series(n, x = 0, y = 1):   
+    defaultArgs = [x == 0, y == 1]
+    optionalArgs = [x == 2, y == 1]
         
-    if all(optionalArgs):
-        return lucas(n)
-    elif all(defaultArgs):
-        return fibonacci(n)
-    else:
-        return "different series"
+    return lucas(n) if all(optionalArgs) else fibonacci(n) if all(defaultArgs) else 'different series' 
+
+    # if all(optionalArgs):
+    #     return lucas(n)
+    # elif all(defaultArgs):
+    #     return fibonacci(n)
+    # else:
+    #     return "different series"
 
 
 # attribution: short circuit eval - https://www.djm.org.uk/posts/python-multiple-line-conditions-and-all-builtin/
