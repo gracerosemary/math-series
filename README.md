ea3854f885ed478f02306a3d59fb8cd82144e42f
Grace Choi <choi.g.330@gmail.com>
2020-12-10 18:13:25 -0800

    lab2 complete

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ PR link:
 
 ## Description
 Feature 1: Create a `fibonacci` function with one parameter `n`. Function should return the nth value in the fibonacci series using recursion or iteration.
-
+Feature 2: Create a `lucas` function with one parameter `n`. Function should return the nth value in the lucas series using recursion or iteration.
+Feature 3: Create a `sum_series` function with one required parameter and two optional parameters. The required parameter will determine which element in the series to print. The two optional parameters will have default values of 0 and 1 and will determine the first two values for the series to be produced.
+Feature 4: Create tests for all functions
 ---
 
 ### Configuration and Getting Started
@@ -13,19 +15,16 @@ Use `poetry` to create `math-series` project.
 Create a module named `series.py`.
 Add a file `test_series.py`.
 
-### Visuals
-#### Application Start
-![Image 1](image.png)  
-#### Using the Application
-![Image 1](image.png)
-#### Application End
-![Image 1](image.png)
-
 ---
 
 ### Change Log
 1.1: *Lab Prep and Testing* - 8 Dec 2020
 1.2: *Lab Feature 1* - 8 Dec 2020
+1.3: *Lab Complete with Fibonacci, Lucas, Sum Series, and Testing* - 9 Dec 2020
+1.4: *ReadMe and DocString Updated* - 10 Dec 2020
 
 ### Credit
 Partners: Sam Clark, Brandon Gonzalez   
+
+### Attribution
+Short circuit eval - https://www.djm.org.uk/posts/python-multiple-line-conditions-and-all-builtin/
--- math_series/series.py
+++ math_series/series.py
@@ -1,38 +1,28 @@
 def fibonacci(n):
+    """Returns None for 0, 0 for 1st sequence, 1 for 2nd sequence, or else returns the nth sequence of the series
+    :param integer (nth sequence of series)
+    :type integer
+    :rtype integer
+    """
     return None if n <= 0 else 0 if n == 1 else 1 if n == 2 else (fibonacci(n-1)+fibonacci(n-2))
-    
-    # if  n <= 0:
-    #     return None
-    # elif n == 1:
-    #     return 0 
-    # elif n == 2:
-    #     return 1
-    # else:
-    #     return (fibonacci(n-1)+fibonacci(n-2))
 
 def lucas(n):
+    """Returns None for 0, 2 for 1st sequence, 1 for 2nd sequence, or else returns the nth sequence of the series
+    :param integer (nth sequence of series)
+    :type integer
+    :rtype integer
+    """
     return None if n <= 0 else 2 if n == 1 else 1 if n == 2 else (lucas(n-1)+lucas(n-2))
-    # if  n <= 0:
-    #     return None
-    # elif n == 1:
-    #     return 2 
-    # elif n == 2:
-    #     return 1
-    # else:
-    #     return (lucas(n-1)+lucas(n-2))
 
 def sum_series(n, x = 0, y = 1):   
+    """Returns nth sequence of lucas series if all optional args are true, nth sequence of fibonacci if all default args are true, else 'different series'
+    :param integer (nth sequence of series), 2nd and 3rd optional
+    :type integer
+    :rtype integer
+    """
     defaultArgs = [x == 0, y == 1]
     optionalArgs = [x == 2, y == 1]
         
     return lucas(n) if all(optionalArgs) else fibonacci(n) if all(defaultArgs) else 'different series' 
-
-    # if all(optionalArgs):
-    #     return lucas(n)
-    # elif all(defaultArgs):
-    #     return fibonacci(n)
-    # else:
-    #     return "different series"
-
 
 # attribution: short circuit eval - https://www.djm.org.uk/posts/python-multiple-line-conditions-and-all-builtin/
